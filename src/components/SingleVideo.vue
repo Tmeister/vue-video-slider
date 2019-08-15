f3296715ef89eeee0618dcb1a783e6dc6a9e7541
Enrique Chavez <noone@tmeister.net>
2019-08-15 14:15:23 -0500

    Slider finish

--- src/components/SingleVideo.vue
+++ src/components/SingleVideo.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="single-video">
-    <img class="placeholder" :src="video.placeholder" :alt="video.title" />
+    <div class="preview" v-if="!isPlaying">
+      <img class="placeholder" :src="video.placeholder" :alt="video.title" />
+      <span class="title">{{video.title}}</span>
+      <svg
+        class="play-btn"
+        @click="handlePlayVideoClick"
+        fill="#ffffff"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        version="1.1"
+        x="0px"
+        y="0px"
+        viewBox="0 0 100 100"
+        style="enable-background:new 0 0 100 100;"
+        xml:space="preserve"
+      >
+        <path
+          d="M50,1C22.9,1,1,22.9,1,50s21.9,49,49,49s49-21.9,49-49S77.1,1,50,1z
+          M50,89.8C28,89.8,10.2,72,10.2,50S28,10.2,50,10.2
+          S89.8,28,89.8,50S72,89.8,50,89.8z M37.8,28.6L74.5,50L37.8,71.4V28.6z"
+        />
+      </svg>
+    </div>
+    <div class="player" v-if="isPlaying">
+      <video width="100%" height="auto" controls autoplay @ended="handleEndVideo">
+        <source :src="video.url" type="video/mp4" />Your browser does not support the video tag.
+      </video>
+    </div>
   </div>
 </template>
 
@@ -13,21 +40,68 @@ export default {
       default: () => {},
     },
   },
+  data() {
+    return {
+      isPlaying: false,
+    };
+  },
+  methods: {
+    handlePlayVideoClick() {
+      this.isPlaying = true;
+    },
+    handleEndVideo() {
+      this.isPlaying = false;
+    },
+    stop() {
+      if (this.isPlaying) {
+        this.isPlaying = false;
+      }
+    },
+  },
 };
 </script>
 
 <style lang="scss">
 .single-video {
-  max-width: 250px;
+  width: 250px;
   border-radius: 5px;
   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);
   background-color: black;
-  display: flex;
   margin-bottom: 20px;
+  margin: 0 auto;
+  position: relative;
+  .preview,
+  .player {
+    display: flex;
+  }
+  .player {
+    video {
+      border-radius: 5px;
+    }
+  }
   .placeholder {
     max-width: 100%;
     border-radius: 5px;
-    opacity: 0.6;
+  }
+  .play-btn {
+    position: absolute;
+    width: 76px;
+    height: 76px;
+    left: 50%;
+    margin-left: -38px;
+    top: 50%;
+    margin-top: -38px;
+    display: none;
+    cursor: pointer;
+  }
+  .title {
+    position: absolute;
+    left: 10px;
+    bottom: 10px;
+    color: white;
+    font-size: 14px;
+    text-transform: uppercase;
+    text-shadow: 2px 2px rgba(0, 0, 0, 0.2);
   }
 }
 </style>
--- src/components/VideoCarousel.vue
+++ src/components/VideoCarousel.vue
@@ -1,21 +1,55 @@
 <template>
   <div class="video-holder">
-    <single-video v-for="video in videos" :key="video.id" :video="video"></single-video>
+    <swiper :options="swiperOption" @slideChange="handleSlideChange">
+      <swiper-slide v-for="video in videos" :key="video.id">
+        <single-video :video="video" :ref="`video-${video.id}`"></single-video>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
+// eslint-disable-next-line
+import 'swiper/dist/css/swiper.css';
 import axios from 'axios';
+import { swiper, swiperSlide } from 'vue-awesome-swiper';
 import SingleVideo from './SingleVideo.vue';
 
 export default {
   name: 'VideoCarousel',
   components: {
     SingleVideo,
+    swiper,
+    swiperSlide,
   },
   data() {
     return {
       videos: [],
+      swiperOption: {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 3,
+        initialSlide: 1,
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 20,
+          depth: 0,
+          modifier: 1,
+          slideShadows: false,
+        },
+        breakpoints: {
+          480: {
+            slidesPerView: 1,
+            coverflowEffect: {
+              stretch: 0,
+            },
+          },
+        },
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      },
     };
   },
   created() {
@@ -28,9 +62,43 @@ export default {
         this.videos = response.data;
       }
     },
+    handleSlideChange() {
+      Object.keys(this.$refs).forEach((player) => {
+        this.$refs[player][0].stop();
+      });
+    },
   },
 };
 </script>
 
-<style scoped lang="scss">
+<style  lang="scss">
+.swiper-inner {
+  width: 100%;
+}
+.swiper-container {
+  padding: 20px 0;
+}
+.swiper-slide {
+  background-position: center;
+  background-size: cover;
+  width: 250px;
+  .single-video {
+    transform: scale(0.85);
+    .placeholder {
+      opacity: 0.6;
+    }
+  }
+}
+.swiper-slide-active {
+  .single-video {
+    transform: scale(1);
+    background-color: transparent;
+    .placeholder {
+      opacity: 1;
+    }
+    .play-btn {
+      display: block;
+    }
+  }
+}
 </style>
